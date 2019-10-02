8172e5b29617ad6ecc2b1b16891789e11ab60ec5
abidex4yemi <abidex4yemi.com>
2019-10-02 10:43:16 +0100

    Complete store exercice

--- src/store/store.py
+++ src/store/store.py
@@ -1,6 +1,4 @@
 # Create a store class with a name and categories
-
-
 class Store:
     def __init__(self, name, categories):
         self.name = name
@@ -15,8 +13,24 @@ class Store:
             output += " " + str(i) + "." + category + "\n"
             i += 1
 
+        output += " " + str(i) + "." + "Press 4 for exit"
         return output
 
 
-my_store = Store("Bob's store", ["shoes", "Hat", "Helicopters"])
+my_store = Store("Bob's store", ["shoes", "Hat", "Helicopters", "Belts"])
 print(my_store)
+
+
+# Create basic interactive commandline app
+selection = 0
+while int(selection) != len(my_store.categories):
+    try:
+        selection = int(input("Select the number of the department."))
+        if selection == len(my_store.categories) + 1:
+            print("Thanks for shopping at" + my_store.name)
+        elif selection > 0 and selection <= len(my_store.categories):
+            print(my_store.categories[selection - 1])
+        else:
+            print("Select a valid number")
+    except ValueError:
+        print("Please enter your choice as a number")
