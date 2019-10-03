244899fe6d8b2393cf28cf1585a8977a9a74a780
abidex4yemi <abidex4yemi.com>
2019-10-03 10:22:34 +0100

    Complete simple composition exercise

--- src/classes/simple_composition.py
+++ src/classes/simple_composition.py
@@ -1,1 +1,19 @@
 # create a Motor class and create a Car class that "Has A" Motor
+
+
+class Motor:
+    def __init__(self, make, cubic_centimeters):
+        self.make = make
+        self.cubic_centimeters = cubic_centimeters
+
+
+class Car:
+    def __init__(self, make, model, engine):
+        self.make = make
+        self.model = model
+        self.engine = engine
+
+
+car = Car("Honda", "civic", Motor("Honder", 120))
+
+print(car.engine.cubic_centimeters)
